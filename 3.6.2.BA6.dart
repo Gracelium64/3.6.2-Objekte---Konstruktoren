81eb91996106c9c28ff5e6dbe90de01940ba9b1c
Grace <grace6424@icloud.com>
2025-03-19 16:03:12 +0100

    ...

--- 3.6.2.BA6.dart
+++ 3.6.2.BA6.dart
@@ -1,19 +1,5 @@
 import 'functions/functions_grace_64.dart';
 
-class Autor {
-  String name;
-  int yearOfBirth;
-
-  Autor(this.name, this.yearOfBirth);
-}
-
-class Book {
-  String titel;
-  String autor;
-  int numberOfPages;
-
-  Book(this.titel, this.autor, this.numberOfPages);
-}
 
 void main() {
 
@@ -27,4 +13,21 @@ void main() {
   print(b1.titel);
   print(b1.numberOfPages);
   print(b1.autor);
+
+  lineBreak();
+}
+
+class Autor {
+  String name;
+  int yearOfBirth;
+
+  Autor(this.name, this.yearOfBirth);
+}
+
+class Book {
+  String titel;
+  String autor;
+  int numberOfPages;
+
+  Book(this.titel, this.autor, this.numberOfPages);
 }
